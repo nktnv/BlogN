6576dcf7e2a43d236b890c902c9f93f650ca3672
nikita <nikitanaryshev@gmail.com>
2017-10-29 18:16:06 +0200

    Added removing images from cloudinary after deleting of images in blog

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,12 +7,12 @@ class ImagesController < ApplicationController
   after_action :clean_public_image_directory
 
   def create
-    if params[:image][:picture]
+    if params[:image][:picture] && image_content_type?(params[:image][:picture])
       file = params[:image][:picture]
       file_extension = File.extname(file.original_filename)
       file.original_filename = random_value + file_extension
       file_path = create_image(absolute_public_image_path(file.original_filename), file.read)
-      file_url = upload_file_to_cloudinary(file_path)
+      file_url = upload_file_to_cloudinary(file_path, random_value(20))
       @image = Image.new(image_params.merge(picture: file_url))
       if @image.save
         redirect_to(user_path(current_user.id))
@@ -29,6 +29,7 @@ class ImagesController < ApplicationController
 
   def destroy
     @image = Image.find(params[:id])
+    delete_file_on_cloudinary(@image.picture)
     @image.destroy
     redirect_to(user_path(current_user.id))
   end
